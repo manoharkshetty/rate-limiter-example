22bd1680f9c03084ef48d1db3d2a8b6e5aa7fe02
Manohar Shetty <manohar.shetty@grabtaxi.com>
2020-01-23 00:03:24 +0800

    Add API layer

---
+++ api/home.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	limiter "github.com/manoharkshetty/rate-limiter-example/rate-limiter"
+)
+
+type HomeHandler struct {
+	rateLimiter limiter.RateLimiter
+}
+
+func New(rateLimiter limiter.RateLimiter) Handler {
+	return HomeHandler{
+		rateLimiter: rateLimiter,
+	}
+}
+
+func (handler HomeHandler) Handle(w http.ResponseWriter, r *http.Request) {
+	user := r.URL.Path[1:]
+	limitReached, tryAfter := handler.rateLimiter.IsLimitReached(context.Background(), user)
+	if limitReached {
+		outputJSON(w, http.StatusTooManyRequests, fmt.Sprintf("Try after %d", tryAfter))
+		return
+	}
+	outputJSON(w, http.StatusOK, "success")
+}
+
+func outputJSON(w http.ResponseWriter, respCode int, payload interface{}) {
+	output, err := json.Marshal(payload)
+	if err != nil {
+		respCode = http.StatusInternalServerError
+	}
+	w.WriteHeader(respCode)
+	_, _ = w.Write(output)
+}
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/manoharkshetty/rate-limiter-example/api"
+	"github.com/manoharkshetty/rate-limiter-example/rate-limiter/config"
+	"github.com/manoharkshetty/rate-limiter-example/rate-limiter/impl"
+	"log"
+	"net/http"
+)
+
+func main() {
+	conf := map[string]*config.Config{
+		"client_1": {
+			MaxReqAllowed:     5,
+			TimeIntervalInSec: 10,
+		},
+		"client_2": {
+			MaxReqAllowed:     5,
+			TimeIntervalInSec: 10,
+		},
+	}
+	rateLimiter := ratelimiterimpl.NewFactory().Get(conf)
+	home := api.New(rateLimiter)
+	http.HandleFunc("/", home.Handle)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+
